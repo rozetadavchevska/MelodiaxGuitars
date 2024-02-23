79c90ca13fe616edf963db697bd13fb987e8b3ed
Rozeta Davchevska <rozetadavcevska@gmail.com>
2024-02-23 16:44:53 +0100

    added service extensions

--- MelodiaxGuitarsAPI/MelodiaxGuitarsAPI/Program.cs
+++ MelodiaxGuitarsAPI/MelodiaxGuitarsAPI/Program.cs
@@ -1,6 +1,10 @@
+using MelodiaxGuitarsAPI.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -12,11 +16,23 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+} 
+else
+{
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
