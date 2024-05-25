7e93657496aee512bb2bdc2c1cf78ef8042f8b84
Rozeta Davchevska <rozetadavcevska@gmail.com>
2024-05-25 19:26:46 +0200

    added filters section

--- MelodiaxGuitarsUI/src/app/components/acoustic-guitars/acoustic-guitars.component.ts
+++ MelodiaxGuitarsUI/src/app/components/acoustic-guitars/acoustic-guitars.component.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatOptionModule } from '@angular/material/core';
-import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider'
+import { Brand } from '../../models/Brand';
+import { Category } from '../../models/Category';
+import { BrandService } from '../../services/brand/brand.service';
+import { CategoryService } from '../../services/category/category.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-acoustic-guitars',
@@ -10,10 +16,31 @@ import { MatSelectModule } from '@angular/material/select';
     MatFormFieldModule,
     MatSelectModule,
     MatOptionModule,
+    MatSliderModule,
+    CommonModule
   ],
   templateUrl: './acoustic-guitars.component.html',
   styleUrl: './acoustic-guitars.component.scss'
 })
-export class AcousticGuitarsComponent {
-  selected = 'option2';
+export class AcousticGuitarsComponent implements OnInit {
+  constructor(
+    private brandService:BrandService,
+    private categoryService:CategoryService
+  ){}
+  brands:Brand[] = [];
+  categories:Category[] = [];
+
+  ngOnInit():void{
+    this.brandService.getBrands().subscribe(
+      (brands) => {
+        this.brands = brands;
+      }
+    )
+
+    this.categoryService.getCategories().subscribe(
+      (categories) => {
+        this.categories = categories;
+      }
+    )
+  }
 }
